399e1ad62525707383ac0ee322afb82be5f4f52d
Прохоренко Софья <sofia.prokhorenko_1@mail.ru>
2025-12-14 20:34:31 +0300

    Пароль
    Добавила пароль для подключения в настройках БД, пароль: "BD_Kursach"

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = 'your-secret-key-here-change-this'
+    SECRET_KEY = 'BD_Kursach'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///time_tracker.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
